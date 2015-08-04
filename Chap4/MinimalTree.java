bf0ca91b9aedab4ac4fc5c71ddbc45e945eeeba7
Andy Hsu <anhsu@groupon.com>
2015-08-03 23:48:32 -0700

    new files

---
+++ Chap4/MinimalTree.java
@@ -0,0 +1,18 @@
+public class MinimalTree{
+	public Node buildMinimalTree(int[] array){
+		return buildMinimalTree(array, 0, array.length-1);
+	}
+
+	public Node buildMinimalTree(int[] array, int start, int end){
+		if(end > start){
+			return null; 
+		}
+		int middle = (start + end)/2;
+		Node node = new Node(array[middle]);
+		Node left = buildMinimalTree(array, start, end-1);
+		node.left = left;
+		Node right = buildMinimalTree(array, start + 1, end);
+		node.right = right;
+		return node;
+	}
+}
